11a0c365a3677009f40ac621f86009738aa66bf4
Fred <fmoore1124@gmail.com>
2021-07-12 20:03:07 -0400

    Updates

---
+++ precache-manifest.8a420d9ffbcf36634ef72632e672614a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0feca7025fa3d6bef4160c9ffe6d82e9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b0e7301052c793cd3191",
+    "url": "/monsters-rolodex/static/css/main.f251e27e.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e7301052c793cd3191",
+    "url": "/monsters-rolodex/static/js/main.45893224.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
